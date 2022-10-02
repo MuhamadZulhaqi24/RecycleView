8554cf9a65c6d5fc0730c71d717a8c20011aa102
MuhamadZulhaqi24 <muhamadzulhaqi0@gmail.com>
2022-10-02 18:37:28 +0700

    Initial commit

---
+++ app/src/main/java/com/ryz/newsnow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ryz.newsnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ryz/newsnow/common/common.kt
@@ -0,0 +1,7 @@
+package com.ryz.newsnow.common
+
+object common{
+    val BASE_URL = "https://newsapi.org/"
+    val API_KEY =  "fdbad69e07224caeba8a0fea5988be5d"
+
+}
